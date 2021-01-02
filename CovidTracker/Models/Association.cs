ad68cf9d1c8bc1ea41bba4f584734fa84cb5ab12
Natty <alecto_perfecto@hotmail.com>
2021-01-01 20:56:50 -0600

    Initial Commit

---
+++ CovidTracker/Models/Association.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CovidTracker.Models
+{
+    public class Association
+    {
+        public int ID { get; set; }
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
